d0a3638b72028d9bfbd74dd0342af88f9b22f6f2
Kaike <kaikedias98@gmail.com>
2024-11-06 23:27:39 -0300

    refactor: change to listTile and using custom theme for text and colors

--- lib/app/menu/ui/components/meal_item.dart
+++ lib/app/menu/ui/components/meal_item.dart
@@ -8,43 +8,36 @@ class MealItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(quantity)
-                  ],
-                ),
-              ),
-            ],
+        ListTile(
+          title: Text(
+            title,
+            style: textTheme.titleMedium,
           ),
-        ),
-        const SizedBox(height: 8),
-        OutlinedButton(
-          onPressed: () {},
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(color: Color(0xFF83C5BE)),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18),
+          subtitle: Text(
+            quantity,
+            style: textTheme.labelLarge
+                ?.copyWith(color: colorScheme.onSurfaceVariant),
+          ),
+          trailing: OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+                side: BorderSide(color: colorScheme.primary),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                padding: const EdgeInsets.all(10)),
+            child: Text(
+              'Substituições',
+              style: textTheme.labelLarge?.copyWith(color: colorScheme.primary),
             ),
           ),
-          child: const Text(
-            'Opções de Substituição',
-            style: TextStyle(color: Color(0xFF83C5BE)),
-          ),
         ),
-        const Divider(),
+        const Divider()
       ],
     );
   }
