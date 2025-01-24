2082a51681cdcf5bd238b2330c137fe4f60e4651
Kaike <kaikedias98@gmail.com>
2025-01-24 15:08:47 -0300

    feat: add routes

--- lib/app/routes/router.dart
+++ lib/app/routes/router.dart
@@ -1,27 +1,52 @@
 import 'package:go_router/go_router.dart';
+import 'package:nutri_app/app/auth/ui/login_page.dart';
+import 'package:nutri_app/app/auth/ui/splash_page.dart';
+import 'package:nutri_app/app/guidelines/ui/guidelines_page.dart';
 import 'package:nutri_app/app/menu/ui/menu_page.dart';
+import 'package:nutri_app/app/personalInfo/ui/personal_info_page.dart';
 import 'package:nutri_app/app/routes/main_layout.dart';
 
-final router = GoRouter(initialLocation: '/', routes: [
+final router = GoRouter(initialLocation: '/splash', routes: [
   GoRoute(
     path: "/",
     name: 'MainLayout',
     builder: (_, __) => const MainLayout(initialTabIndex: 0),
     routes: [
-      GoRoute(
-        path: 'menu',
-        builder: (_, __) => const MenuPage(),
-      ),
-    ],
+    GoRoute(
+      path: 'menu',
+      name: 'menu',
+      builder: (_, __) => const MenuPage(),
+    ),
+    GoRoute(
+      path: 'home',
+      name: 'home',
+      builder: (_, __) => const MainLayout(initialTabIndex: 1),
+    ),
+    GoRoute(
+      path: 'goals',
+      name: 'goals',
+      builder: (_, __) => const MainLayout(initialTabIndex: 2),
+    ),
+  ],
   ),
   GoRoute(
-    path: '/home',
-    name: 'home',
-    builder: (_, __) => const MainLayout(initialTabIndex: 1),
+    path: '/login',
+    name: 'login',
+    builder: (_, __) => const LoginPage(),
   ),
   GoRoute(
-    path: '/goals',
-    name: 'goals',
-    builder: (_, __) => const MainLayout(initialTabIndex: 2),
+    path: '/splash',
+    name: 'splash',
+    builder: (context, state) => const SplashPage(),
+  ),
+  GoRoute(
+    path: '/guidelines',
+    name: 'guidelines',
+    builder: (context, state) => const GuidelinesPage(),
+  ),
+  GoRoute(
+    path: '/personalInfo',
+    name: 'personalInfo',
+    builder: (context, state) => const PersonalInfoPage(),
   )
 ]);
